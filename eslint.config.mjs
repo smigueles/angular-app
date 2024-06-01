5e6b068856b82b28923d287f7eebced7e9d576ad
smigueles <samiramiguelesdiaz@gmail.com>
2024-06-01 00:12:42 -0300

    fix: linter

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "node_modules/",
+      "e2e/",
+      "dist/",
+      ".angular/",
+      "*.test.js",
+      "*.spec.js",
+    ],
+  }
+);
