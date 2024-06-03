0de4204e63bada39475854622247694d1e08b732
smigueles <samiramiguelesdiaz@gmail.com>
2024-06-03 03:42:08 -0300

    fix: credentials

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
+    apiKey: 'AIzaSyDoiO5UO0E0BuOsEX99J7awnRBpsm6h1Uk',
+    authDomain: 'angular-app-78808.firebaseapp.com',
+    projectId: 'angular-app-78808',
+    storageBucket: 'angular-app-78808.appspot.com',
+    messagingSenderId: '822002023467',
+    appId: '1:822002023467:web:19aea8bdce37a90ed57e6e',
+    measurementId: 'G-59HJVCB357',
   },
 };
